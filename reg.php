9798dd6c07290b143cf51d5e524ec42295b4c921
jaimini15 <shahjaimini2005@gmail.com>
2025-09-14 16:36:58 +0530

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,24 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "Jaimini@2005";
+    $dbname = "gift_shop";
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+
+    if($conn->connect_error)
+    {
+        echo "Connection failed";
+    }
+
+    $fname = $_POST['fname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $adrs1 = $_POST['adrs1'];
+    $adrs2 = $_POST['adrs2'];
+    $city = $_POST['city'];
+    $pincode = $_POST['pincode'];
+
+    $sql = "INSERT INTO user_details (Fname,Email,`Password`,`Address line 1`,`Adsress line 2`,City,Pincode) VALUES ('$fname','$email','$password','$adrs1')"
+
+?>
