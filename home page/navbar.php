f83a6d2a93b3cf7ce78246f9db8698385dd9f17c
prernanankani123 <prernanankani130@gmail.com>
2025-10-16 09:37:56 +0530

    Create navbar.php

---
+++ home page/navbar.php
@@ -0,0 +1,18 @@
+<!-- HEADER -->
+  <header>
+    <div class="logo">GiftShop</div>
+    <nav>
+      <ul>
+        <li><a href="#" class="active">Home</a></li> |
+        <li><a href="#">Shop</a></li> | 
+        <li><a href="#">About us</a></li> |
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="icons">
+      <a href="#"><i class="fa-solid fa-cart-shopping"></i>Cart</a>
+      <a href="#"><i class="fa-regular fa-user"></i> My Profile</a>
+    </div>
+  </header>
+
+  <!-- HERO SECTION -->
