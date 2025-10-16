b36151c753f75973cda4dc5b655c82458dbf0966
prernanankani123 <prernanankani130@gmail.com>
2025-10-16 09:37:52 +0530

    Create index.php

---
+++ home page/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>GiftShop | Home</title>
+  <link rel="stylesheet" href="style.css" />
+  
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+    <?php include 'navbar.php'; ?> 
+  <section class="hero">
+    <div class="hero-left">
+      <img src="homephoto2.jpg" alt="Decor Image" class="hero-img">
+    </div>
+    <div class="hero-content">
+      <p class="collection">New Collection</p>
+      <h1>Afforable and quanlity products</h1>
+      <p class="subtitle">Supporting local makers since 2025</p>
+      <button class="shop-btn">SHOP NOW</button>
+    </div>
+    <div class="hero-right">
+      <img src="homephoto1.jpg" alt="Decor Image" class="hero-img">
+    </div>
+  </section>
+
+  <!-- FEATURES SECTION -->
+  <section class="features">
+    <h2>Our Amazing Feature</h2>
+    <div class="feature-container">
+      <div class="feature">
+        <h3>100k Satisfied Customers</h3>
+        <p>Nam liber tempor cum soluta nobis eleifend option congue.</p>
+      </div>
+      <div class="feature">
+        <h3>Expert Customer Service</h3>
+        <p>Nam liber tempor cum soluta nobis eleifend option congue.</p>
+      </div>
+      <div class="feature">
+        <h3>Fast & Free Shipping</h3>
+        <p>Nam liber tempor cum soluta nobis eleifend option congue.</p>
+      </div>
+    </div>
+  </section>
+
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</body>
+</html>
