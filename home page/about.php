276826ea40f0224196f2fbd0c3cab497dc74ea2c
jaimini15 <shahjaimini2005@gmail.com>
2025-10-16 19:39:47 +0530

    Update about.php

--- home page/about.php
+++ home page/about.php
@@ -9,7 +9,33 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
-    <?php require_once 'navbar.php' ?>
+    <!-- HEADER -->
+  <header>
+    <div class="logo">GiftShop</div>
+    <nav>
+      <ul>
+        <li><a href="index.php" >Home</a></li> |
+        <li><a href="about.php" class="active">About us</a></li> | 
+        <li class="dropdown">
+        <a href="#">Shop</a>
+        <ul class="dropdown-content">
+          <li><a href="mug.html">Mug</a></li>
+          <li><a href="frame.html">Frame</a></li>
+          <li><a href="cover.html">Mobile Cover</a></li>
+          <li><a href="diary.html">Dairies</a></li>
+        </ul>
+      </li> |
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="icons">
+      <a href="#"><i class="fa-solid fa-cart-shopping"></i>Cart</a>
+      <a href="#"><i class="fa-regular fa-user"></i> My Profile</a>
+    </div>
+  </header>
+
+  <!-- HEADER SECTION ENDS HERE-->
+   
 <section class="about-section">
   <div class="about-container">
 
