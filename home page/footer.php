405008cf81211b211a4c8392aef161de9f1e182a
prernanankani123 <prernanankani130@gmail.com>
2025-10-16 15:58:18 +0530

    Create footer.php

---
+++ home page/footer.php
@@ -0,0 +1,40 @@
+<!--fotter section starts here-->
+    <section class="footer">
+        <div class="box-container">
+
+                <div class="box">
+                    <h3>Quick links</h3>
+                    <a href="home.html"><i class="fas fa-angle-right"></i>Home</a>
+                    <a href="about.html"><i class="fas fa-angle-right"></i>About Us</a>
+                    <a href="package.html"><i class="fas fa-angle-right"></i>Shop</a>
+                    <a href="contact_us.html"><i class="fas fa-angle-right"></i>Contact us</a>
+                </div>
+
+                <div class="box">
+                    <h3>Extra links</h3>
+                    <a href="#"><i class="fas fa-angle-right"></i>Ask qusetion</a>
+                    <a href="#"><i class="fas fa-angle-right"></i>Privacy policy</a>
+                    <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
+                </div>
+                
+                <div class="box">
+                    <h3>Contact info</h3>
+                    <a href="#"><i class="fas fa-phone"></i>+123-456-7890</a>
+                    <a href="#"><i class="fas fa-phone"></i>+222-333-4523</a>
+                    <a href="#"><i class="fas fa-envelope"></i>GiftShop@gmail.com</a>
+                    <a href="#"><i class="fas fa-map"></i>Maninagar, India - 380008</a>
+                </div>
+
+                <div class="box">
+                    <h3>Follow us</h3>
+                    <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+                    <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+                    <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+                    <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+                    
+                </div>
+                
+        </div>
+        <div class="credit">created by <span>GiftShop</span> | all right reserved!</div>
+    </section>
+    <!--fotter section ends here-->
