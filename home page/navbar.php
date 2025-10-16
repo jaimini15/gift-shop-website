751667b5894913414c4a2dc0d33a382c048efd96
prernanankani123 <prernanankani130@gmail.com>
2025-10-16 15:58:25 +0530

    Update navbar.php

--- home page/navbar.php
+++ home page/navbar.php
@@ -5,7 +5,15 @@
       <ul>
         <li><a href="#" class="active">Home</a></li> |
         <li><a href="#">About us</a></li> | 
-        <li><a href="#">Shop</a></li> |
+        <li class="dropdown">
+        <a href="#">Shop</a>
+        <ul class="dropdown-content">
+          <li><a href="mug.html">Mug</a></li>
+          <li><a href="frame.html">Frame</a></li>
+          <li><a href="cover.html">Mobile Cover</a></li>
+          <li><a href="diary.html">Dairies</a></li>
+        </ul>
+      </li> |
         <li><a href="#">Contact</a></li>
       </ul>
     </nav>
@@ -16,3 +24,4 @@
   </header>
 
   <!-- HERO SECTION -->
+   
