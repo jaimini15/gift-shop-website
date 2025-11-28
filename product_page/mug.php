a76df717dfa96325f99d9d693f03a738c9bc9196
jaimini15 <shahjaimini2005@gmail.com>
2025-11-28 12:50:58 +0530

    Update mug.php

--- product_page/mug.php
+++ product_page/mug.php
@@ -10,7 +10,29 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
-<?php include '../home page/navbar.php'; ?>
+<header>
+    <div class="logo">GiftShop</div>
+    <nav>
+      <ul>
+        <li><a href="index.php" >Home</a></li> |
+        <li><a href="../home page/about.php" >About us</a></li> | 
+        <li class="dropdown">
+        <a href="#" class="active">Shop</a>
+        <ul class="dropdown-content">
+          <li><a href="mug.html">Mug</a></li>
+          <li><a href="frame.html">Frame</a></li>
+          <li><a href="cover.html">Mobile Cover</a></li>
+          <li><a href="diary.html">Dairies</a></li>
+        </ul>
+      </li> |
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="icons">
+      <a href="#"><i class="fa-solid fa-cart-shopping"></i>Cart</a>
+      <a href="#"><i class="fa-regular fa-user"></i> My Profile</a>
+    </div>
+  </header>
 
 
     <section class="hero-title">
