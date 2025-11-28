dd79fed820948889ace6c15ab43bfa482cae609d
prernanankani123 <prernanankani130@gmail.com>
2025-11-28 15:14:43 -0800

    Create category_section.php

---
+++ home page/category_section.php
@@ -0,0 +1,41 @@
+<section class="home-package" id="categories">
+    <h1 class="heading-title">Our Categories</h1>
+    <div class="box-container">
+      <div class="box">
+        <div class="image">
+          <img src="home_category_mug.jpg">
+        </div>
+        <div class="content">
+          <h3>Mug</h3>
+          <a href="contact_us.html" class="btn">Explore</a>
+        </div>
+      </div>
+      <div class="box">
+        <div class="image">
+          <img src="home_category_mobile.jpg">
+        </div>
+        <div class="content">
+          <h3>Mobile Cover</h3>
+          <a href="contact_us.html" class="btn">Explore</a>
+        </div>
+      </div>
+      <div class="box">
+        <div class="image">
+          <img src="home_category_frame.jpg">
+        </div>
+        <div class="content">
+          <h3>Photo Frame</h3>
+          <a href="contact_us.html" class="btn">Explore</a>
+        </div>
+      </div>
+      <div class="box">
+        <div class="image">
+          <img src="home_category_book.jpg">
+        </div>
+        <div class="content">
+          <h3>Dairies</h3>
+          <a href="contact_us.html" class="btn">Explore</a>
+        </div>
+      </div>
+    </div>
+</section>
