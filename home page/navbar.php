2791f9a2f4ff8a4774ca26a25085fcf0a3eaf6d1
prernanankani123 <prernanankani130@gmail.com>
2025-11-29 17:24:17 -0800

    Update navbar.php

--- home page/navbar.php
+++ home page/navbar.php
@@ -14,7 +14,7 @@
           <li><a href="../product_page/diaries.php">Dairies</a></li>
         </ul>
       </li> |
-        <li><a href="#">Contact</a></li>
+        <li><a href="../contact us/contact.php">Contact</a></li>
       </ul>
     </nav>
     <div class="icons">
