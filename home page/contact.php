7d75c19e9e83bbf395bc5dca05d9c81dd3fd1742
prernanankani123 <prernanankani130@gmail.com>
2025-11-30 15:52:51 -0800

    Update contact.php

--- home page/contact.php
+++ home page/contact.php
@@ -12,29 +12,46 @@
 </head>
 
 <body>
+  <?php include("../AdminPanel/db.php"); ?>
 <header>
     <div class="logo">GiftShop</div>
+
     <nav>
       <ul>
-       <li><a href="index.php" >Home</a></li> |
-        <li><a href="about.php" >About us</a></li>| 
+        <li><a href="../home page/index.php">Home</a></li> |
+        <li><a href="about.php">About us</a></li> | 
+        
         <li class="dropdown">
-        <a href="#">Shop</a>
-        <ul class="dropdown-content">
-         <li><a href="../product_page/mug.php">Mug</a></li>
-          <li><a href="../product_page/frame.php">Frame</a></li>
-          <li><a href="../product_page/mobilecover.php">Mobile Cover</a></li>
-          <li><a href="../product_page/diaries.php">Dairies</a></li>
-        </ul>
-      </li> |
+          <a href="#">Shop</a>
+
+          <ul class="dropdown-content">
+
+            <?php  
+              $catQuery = "SELECT * FROM category_details WHERE Status='Enabled'";
+              $catResult = mysqli_query($connection, $catQuery);
+
+              while ($cat = mysqli_fetch_assoc($catResult)) {
+            ?>
+                <li>
+                  <a href="../product_page/product_list.php?category_id=<?= $cat['Category_Id'] ?>">
+                    <?= $cat['Category_Name'] ?>
+                  </a>
+                </li>
+            <?php } ?>
+
+          </ul>
+        </li> |
+
         <li><a href="contact.php" class="active">Contact</a></li>
       </ul>
     </nav>
+
     <div class="icons">
-      <a href="#"><i class="fa-solid fa-cart-shopping"></i>Cart</a>
+      <a href="#"><i class="fa-solid fa-cart-shopping"></i> Cart</a>
       <a href="#"><i class="fa-regular fa-user"></i> My Profile</a>
     </div>
-  </header>
+</header>
+
     <!-- Main Contact Section -->
     <div class="contact-main">
 
