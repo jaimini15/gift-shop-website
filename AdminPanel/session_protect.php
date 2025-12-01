d96327daef8479cfcf869eabb78f6af93908a310
prernanankani123 <prernanankani130@gmail.com>
2025-11-30 16:52:26 -0800

    Update session_protect.php

--- AdminPanel/session_protect.php
+++ AdminPanel/session_protect.php
@@ -5,6 +5,6 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // Check if admin logged in
 if (!isset($_SESSION['admin_logged_in'])) {
-    header("Location: ../login/login.php");
+    header("Location: ../../login/login.php");
     exit();
 }
