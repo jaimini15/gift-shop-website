aad8ff82bb01c6236dbc738b9f11cb3253e581f6
jaimini15 <shahjaimini2005@gmail.com>
2025-12-03 15:55:07 +0530

    Update session_protect.php

--- AdminPanel/session_protect.php
+++ AdminPanel/session_protect.php
@@ -1,8 +1,8 @@
 <?php
-if (!isset($_SESSION)) session_start();
+session_start();
 
 if (!isset($_SESSION['admin_logged_in'])) {
-    header("Location: AdminPanel/admin login/login.php");
+    header("Location: admin login/login.php");
     exit();
 }
 ?>
