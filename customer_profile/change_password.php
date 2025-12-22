4e1ac577e3e7c98b14378883bec03c16bd076c47
prernanankani123 <prernanankani130@gmail.com>
2025-12-22 11:36:48 -0800

    Update change_password.php

--- customer_profile/change_password.php
+++ customer_profile/change_password.php
@@ -13,18 +13,20 @@ include("account_layout.php");
 
 <h2>Change Password</h2>
 
+<?php if (isset($_GET['success'])): ?>
+    <p style="color:green;">Password updated successfully ✅</p>
+<?php endif; ?>
 <?php if (isset($_GET['error'])): ?>
     <p style="color:red;">
         <?php
             if ($_GET['error'] == 1) echo "Current password is incorrect";
             if ($_GET['error'] == 2) echo "New passwords do not match";
+            if ($_GET['error'] == 3) echo "You already have this password";
+            if ($_GET['error'] == 4) echo "User not found";
         ?>
     </p>
 <?php endif; ?>
 
-<?php if (isset($_GET['success'])): ?>
-    <p style="color:green;">Password updated successfully ✅</p>
-<?php endif; ?>
 
 <form method="post" action="update_password.php">
 
