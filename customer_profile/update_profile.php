ce5c8242ba8a96fa02070de5625d6e6109215acc
prernanankani123 <prernanankani130@gmail.com>
2025-12-21 18:43:32 -0800

    Create update_profile.php

---
+++ customer_profile/update_profile.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include("../AdminPanel/db.php");
+
+if (!isset($_SESSION["User_Id"])) {
+    header("Location: ../login/login.php");
+    exit();
+}
+
+$uid = $_SESSION["User_Id"];
+
+$fname   = mysqli_real_escape_string($connection, $_POST['fname']);
+$lname   = mysqli_real_escape_string($connection, $_POST['lname']);
+$phone   = mysqli_real_escape_string($connection, $_POST['phone']);
+$address = mysqli_real_escape_string($connection, $_POST['address']);
+$pincode = mysqli_real_escape_string($connection, $_POST['pincode']);
+$dob     = mysqli_real_escape_string($connection, $_POST['dob']);
+
+mysqli_query($connection, "
+    UPDATE user_details SET
+        First_Name='$fname',
+        Last_Name='$lname',
+        Phone='$phone',
+        Address='$address',
+        Pincode='$pincode',
+        DOB='$dob'
+    WHERE User_Id='$uid'
+");
+
+header("Location: profile.php?updated=1");
+exit();
