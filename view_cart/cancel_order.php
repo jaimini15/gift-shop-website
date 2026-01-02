4a237951a7f77b9b958b2fa2de27aef2c39b9100
prernanankani123 <prernanankani130@gmail.com>
2026-01-02 19:26:45 +0530

    Update cancel_order.php

--- view_cart/cancel_order.php
+++ view_cart/cancel_order.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
-include("../AdminPanel/db.php");
 header('Content-Type: application/json');
+error_reporting(0);
+ini_set('display_errors', 0);
+
+include("../AdminPanel/db.php");
+
 
 /* =======================
    AUTH CHECK
