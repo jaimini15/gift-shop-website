db08967e0eec6063f05150b06c73a5b9ccf9e558
prernanankani123 <prernanankani130@gmail.com>
2026-01-04 16:25:09 +0530

    Update cancel_order.php

--- view_cart/cancel_order.php
+++ view_cart/cancel_order.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
-header('Content-Type: application/json');
-error_reporting(0);
-ini_set('display_errors', 0);
-
 include("../AdminPanel/db.php");
-
+header('Content-Type: application/json');
 
 /* =======================
    AUTH CHECK
