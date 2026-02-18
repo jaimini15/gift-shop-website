2b00911d38155104bfbeb8f06e097b8bfdebc9a4
prernanankani123 <prernanankani130@gmail.com>
2026-02-18 22:44:29 +0530

    Update registration.php

--- registration/registration.php
+++ registration/registration.php
@@ -70,6 +70,10 @@ if (isset($_SESSION['User_Id'])) {
 }
 
 </style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.0/fabric.min.js"></script>
 </head>
 <body>
 
