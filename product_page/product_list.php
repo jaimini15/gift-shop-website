233026cf8a563556d820a21c94848878b7418f94
jaimini15 <shahjaimini2005@gmail.com>
2026-03-17 18:06:43 +0530

    Update product_list.php

--- product_page/product_list.php
+++ product_page/product_list.php
@@ -52,7 +52,7 @@ function img_src_from_blob($blob, $placeholder = '../product_page/product_mug_bu
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?= $categoryName ?> | GiftShop</title>
+<title>GiftShop | <?= $categoryName ?></title>
 
 <link rel="stylesheet" href="../home page/style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
