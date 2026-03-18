e2227db643554ee3fc0993d778413e931dd68e10
prernanankani123 <prernanankani130@gmail.com>
2026-03-19 00:05:04 +0530

    Update delivery.php

--- AdminPanel/delivery/delivery.php
+++ AdminPanel/delivery/delivery.php
@@ -25,7 +25,7 @@ include(__DIR__ . '/../db.php');
 <div class="content">
 <div class="card-box">
 
-<h3 style="font-size:26px;font-weight:bold;margin-bottom:25px;">Delivered Orders </h3>
+<h3 style="font-size:26px;font-weight:bold;margin-bottom:25px;">Delivered Order</h3>
 
 <?php
 $deliveries = mysqli_query($connection, "
