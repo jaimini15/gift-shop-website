7b3e8d2ef0de7a26cad8f88ad0e45912f7d3a99f
prernanankani123 <prernanankani130@gmail.com>
2026-03-25 19:07:31 +0530

    Update footer.php

--- home page/footer.php
+++ home page/footer.php
@@ -1,41 +1,5 @@
 <!--fotter section starts here-->
     <section class="footer">
-        <!-- <div class="box-container">
-
-                <div class="box">
-                    <h3>Quick links</h3>
-                    <a href="../home page/index.php"><i class="fas fa-angle-right"></i>Home</a>
-                    <a href="../home page/about.php"><i class="fas fa-angle-right"></i>About Us</a>
-                    <a href="../home page/index.php#categories"><i class="fas fa-angle-right"></i>Shop</a>
-                    <a href="../home page/contact.php"><i class="fas fa-angle-right"></i>Contact us</a>
-                </div>
-
-                <div class="box">
-                    <h3>Extra links</h3>
-                    <a href="#"><i class="fas fa-angle-right"></i>Ask question</a>
-                    <a href="#"><i class="fas fa-angle-right"></i>Privacy policy</a>
-                    <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
-                </div>
-                
-                <div class="box">
-                    <h3>Contact info</h3>
-                    <a href="#"><i class="fas fa-phone"></i>+123-456-7890</a>
-                    <a href="#"><i class="fas fa-phone"></i>+222-333-4523</a>
-                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=giftshopmaninagar@gmail.com" target="_blank">
-                    <i class="fas fa-envelope"></i> giftshopmaninagar@gmail.com</a>
-                    <a href="#"><i class="fas fa-map"></i>Maninagar, India - 380008</a>
-                </div>
-
-                <div class="box">
-                    <h3>Follow us</h3>
-                    <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
-                    <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
-                    <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
-                    <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
-                    
-                </div> -->
-                
-        <!-- </div> -->
         <div class="credit">&copy; 2025 <span>GiftShop</span> | all right reserved!</div>
     </section>
     <!--fotter section ends here-->
